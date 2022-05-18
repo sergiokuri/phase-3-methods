dedbbfe0962e72a59a7e1d11f7f5935afa34b9c0
Sergio <kurisergio777777@gmail.com>
2022-05-18 13:56:53 -0400

    commit tree 84f4f74f5c7478e52f7bd5426e717d207d2661be

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+gret(Naureen)
+
+def greetWithDefault(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greetWithDefault(Sunny)
+
+def add(num1, num2)
+    num1 + num2
+end
+
+add (1, 2)
+
+def halve (number)
+    if number.class != integer
+        return nill
+    else number / 2; 
+
+halve (2)
